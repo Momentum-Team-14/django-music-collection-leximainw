f0c1f084e4f694029f72edf4d5ef730f309aaef1
Alexis Mainwaring <leximainw@gmail.com>
2022-08-23 18:52:04 -0400

    create views for albums and artists

--- albums/urls.py
+++ albums/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.album_list, name='Album list')
+    path('', views.album_list),
+    path('albums/', views.album_list, name='Album list'),
+    path('albums/<int:pk>/', views.album_details, name='Album details'),
+    path('artists/', views.artist_list, name='Artist list'),
+    path('artists/<int:pk>/', views.artist_details, name='Artist details'),
 ]
--- albums/views.py
+++ albums/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
-from .models import Album
+from django.shortcuts import (
+    get_object_or_404,
+    render,
+)
+from .models import (
+    Album,
+    Artist,
+)
+
 
 def album_list(request):
     albums = Album.objects.all()
     return render(request, 'albums/album_list.html', {'albums': albums})
+
+
+def album_details(request, pk=None):
+    album = get_object_or_404(Album, pk=pk)
+    return render(request, 'albums/album_details.html', {'album': album})
+
+
+def artist_list(request):
+    artists = Artist.objects.all()
+    return render(request, 'albums/artist_list.html', {'artists': artists})
+
+
+def artist_details(request, pk=None):
+    artist = get_object_or_404(Artist, pk=pk)
+    return render(request, 'albums/artist_details.html', {'artist': artist})
