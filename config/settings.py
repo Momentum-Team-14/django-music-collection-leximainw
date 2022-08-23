0bb5cfd16c8a24a470052a16057109cca8e70a2f
Alexis Mainwaring <leximainw@gmail.com>
2022-08-23 16:50:38 -0400

    add context processor to provide site name to all templates

--- config/settings.py
+++ config/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SITE_NAME = 'Django Music'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -63,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'albums.context_processors.site_name',
             ],
         },
     },
